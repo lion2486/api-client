c9c121dfb9fae408d85d2bdc02a73fbd2afa10d5
lion2486 <lion_2486@yahoo.gr>
2021-04-24 18:20:59 +0300

    handling all 2XX http codes as success

--- src/Request.php
+++ src/Request.php
@@ -48,7 +48,7 @@ class Request
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
-        if ($httpCode === 200) {
+        if ($httpCode >= 200 && $httpCode <= 299) {
             if (!empty($response)) {
                 if($plain) { 
                     return [$response];
